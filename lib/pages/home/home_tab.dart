84fb1db5bc5c5d53bfc86d23e4b4dfcb7683306b
babayaga2002 <csidharth@iitg.ac.in>
2022-06-26 12:19:17 +0530

    Map Box Merge

--- lib/pages/home/home_tab.dart
+++ lib/pages/home/home_tab.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:onestop_dev/globals.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:onestop_dev/globals/my_colors.dart';
+import 'package:onestop_dev/globals/my_fonts.dart';
 import 'package:onestop_dev/models/timetable/registered_courses.dart';
 import 'package:onestop_dev/stores/login_store.dart';
+import 'package:onestop_dev/stores/mapbox_store.dart';
 import 'package:onestop_dev/stores/timetable_store.dart';
 import 'package:onestop_dev/widgets/home/date_course.dart';
 import 'package:onestop_dev/widgets/home/quick_links.dart';
 import 'package:onestop_dev/widgets/mapBox.dart';
 import 'package:provider/provider.dart';
+import 'package:shimmer/shimmer.dart';
 
-double lat = userlat;
-double long = userlong;
 
 class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
@@ -19,22 +21,13 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-  int selectedIndex = 0;
-  String sel = '';
-  Future<RegisteredCourses>? timetable;
 
+  Future<RegisteredCourses>? timetable;
   void initState() {
     super.initState();
     print("Init state");
     context.read<TimetableStore>().setTimetable(
         context.read<LoginStore>().userData["rollno"] ?? "190101109");
-  }
-
-  void rebuildParent(int newSelectedIndex) {
-    print('Reloaded');
-    setState(() {
-      selectedIndex = newSelectedIndex;
-    });
   }
 
   @override
@@ -46,17 +39,16 @@ class _HomeTabState extends State<HomeTab> {
           SizedBox(
             height: 10,
           ),
-          MapBox(
-            lat: (lat != 0) ? lat : null,
-            long: (long != 0) ? long : null,
-            selectedIndex: selectedIndex,
-            rebuildParent: rebuildParent,
-            istravel: true,
+          Builder(
+              builder: (context) {
+                context.read<MapBoxStore>().checkTravelPage(false);
+                return MapBox();
+              }
           ),
           SizedBox(
             height: 10,
           ),
-          DateCourse(),
+          DateCourse(), // <-Put all UI and Observer within DateCourse()
           SizedBox(
             height: 10,
           ),
